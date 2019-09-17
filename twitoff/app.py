37aa09bddd2959a59f13e1f889f185ae9375be1c
Harsh Desai <hrdesai007@gmail.com>
2019-09-17 02:18:22 -0500

    :beers:

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,17 +1,19 @@
 """Main application and routing logic for TwitOff."""
-
-from flask import Flask, render_template
-from .models import db
+from decouple import config
+from flask import Flask, render_template, request
+from .models import db, User
 
 
 def create_app():
     """Create and configure and instance of flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV')
     db.init_app(app)
 
     @app.route('/')
-    def home():
-        return render_template('home.html')
+    def index():
+        users = User.query.all()
+        return render_template('index.html', title='Home', users=users)
 
     return app
