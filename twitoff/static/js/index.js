578fb78ef83963899dd6ef95b0cf4d40eb1faba4
kyle Guerrero <kyleguerrero85@gmail.com>
2020-03-08 17:19:40 -0500

    chore: reorganized compare user function

--- twitoff/static/js/index.js
+++ twitoff/static/js/index.js
@@ -22,20 +22,13 @@ $(document).ready(() => {
 
 function compareUsers(e){
     e.preventDefault(); //prevent default action
-    let post_url = $(this).attr("action"); //get form action url
-    let request_method = $(this).attr("method"); //get form GET/POST method
-    let tweet_text = $(this.tweet_text).val();
-    let user1 = $(this.user1).val();
-    let user2 = $(this.user2).val();
-    let post_data = JSON.stringify({
-        "user1": user1,
-        "user2": user2,
-        "tweet_text": tweet_text
-    })
 
-    let error_msg = '';
-    let compare_result = $(".compare-result");
-    let msg = '';
+    const user1 = $(this.user1).val();
+    const user2 = $(this.user2).val();
+    let error_msg;
+
+    // form message container
+    const compare_result = $(".compare-result");
 
     if (user1 === null || user2 === null) {
         error_msg +=
@@ -58,20 +51,29 @@ function compareUsers(e){
         compare_result.html(error_msg)
         setTimeout(() => compare_result.html(''), 2.5 * 1000);
     }
-
     else {
+
+        const post_url = $(this).attr("action"); //get form action url
+        const request_method = $(this).attr("method"); //get form GET/POST method
+        const tweet_text = $(this.tweet_text).val();
+        const post_data = JSON.stringify({
+            "user1": user1,
+            "user2": user2,
+            "tweet_text": tweet_text
+        })
+        
         $.ajax({
             url: post_url,
             type: request_method,
             data: post_data,
             success: function (results) {
-                msg += `
+                const msg =  `
                 <div class="alert alert-info text-center" role="alert">
                 <p>${results.message}</p>
             </div>
                 `
                 compare_result.html(msg);
-                setTimeout(() => compare_result.html(''), 5.5 * 1000);
+                setTimeout(() => compare_result.html(''), 10 * 1000);
             },
             error: function () {
                 return
