3eb174f9e975b38b88a36d7d605af3878e32c09a
Harsh Desai <hrdesai007@gmail.com>
2020-03-08 02:32:56 +0000

    user routes updated

--- twitoff/routes/user_routes.py
+++ twitoff/routes/user_routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, request
+from flask import render_template, Blueprint, request, jsonify
 from twitoff.twitter import add_or_update_user, update_all_users, add_users, TWITTER_USERS
 from twitoff.models import db, User, Tweet
 
@@ -6,25 +6,27 @@ user_routes = Blueprint("user_routes", __name__)
 
 
 @user_routes.route('/user', methods=['POST'])
-def add_user(error_message=None, success_message=None):
+def add_user(message=None):
     try:
-        username = request.form.get('username')
+        data = request.get_json(force=True)
+        username = data['username']
         users = User.query.all()
         user_handle_list = [user.handle for user in users]
         if username in user_handle_list:
-            success_message = f'User {username} already added, updating user tweets!'
+            message = f'User {username} already added, updating user tweets!'
             add_or_update_user(username)
-            print(f'[DEBUG] {success_message}')
+            print(f'[DEBUG] {message}')
         else:
             add_or_update_user(username)
-            success_message = "User {} successfully added!".format(
+            message = "User {} successfully added!".format(
                 username)
-            print(f'[DEBUG] {success_message}')
+            print(f'[DEBUG] {message}')
+        success = True
     except Exception as e:
-        error_message = "Error adding {}: {}".format(username, e)
-        print(f'[DEBUG] {error_message}')
-        tweets = []
-    return render_template("predictions.html", title='Predictions', users=users, success_message=success_message, error_message=error_message)
+        message = "Error adding {}: {}".format(username, e)
+        print(f'[DEBUG] {message}')
+        success = False
+    return jsonify({'success': success, 'message': message})
 
 
 @user_routes.route('/user/<username>', methods=['GET'])
@@ -45,3 +47,18 @@ def reset():
     db.drop_all()
     db.create_all()
     return render_template('index.html', title='Reset - Database!', users=[])
+
+
+@user_routes.route('/delete/<username>', methods=['DELETE'])
+def delete_user(username):
+    try:
+        user = User.query.filter(User.handle == username).one()
+        db.session.delete(user)
+        db.session.commit()
+        success = True
+        message = f'{user } has been sucessfully deleted.'
+        print(message)
+    except Exception as e:
+        success = False
+        message = "User not found."
+    return jsonify({'success': success, 'message': message})
